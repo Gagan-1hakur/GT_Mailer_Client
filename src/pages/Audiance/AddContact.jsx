08a56585502df8b67193fc20c6fc14606acb40c4
Gagan-1hakur <gaganthakur750@gmail.com>
2024-11-13 15:46:13 +0530

    First Upload

---
+++ src/pages/Audiance/AddContact.jsx
@@ -0,0 +1,107 @@
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+// import CheckboxFive from '../../components/Checkboxes/CheckboxFive';
+// import CheckboxFour from '../../components/Checkboxes/CheckboxFour';
+// import CheckboxOne from '../../components/Checkboxes/CheckboxOne';
+// import CheckboxThree from '../../components/Checkboxes/CheckboxThree';
+// import CheckboxTwo from '../../components/Checkboxes/CheckboxTwo';
+// import SwitcherFour from '../../components/Switchers/SwitcherFour';
+// import SwitcherOne from '../../components/Switchers/SwitcherOne';
+// import SwitcherThree from '../../components/Switchers/SwitcherThree';
+// import SwitcherTwo from '../../components/Switchers/SwitcherTwo';
+// import DatePickerOne from '../../components/Forms/DatePicker/DatePickerOne';
+// import DatePickerTwo from '../../components/Forms/DatePicker/DatePickerTwo';
+// import SelectGroupTwo from '../../components/Forms/SelectGroup/SelectGroupTwo';
+// import MultiSelect from '../../components/Forms/MultiSelect';
+
+const FormElements = () => {
+  return (
+    <>
+      <Breadcrumb pageName="Form Elements" />
+
+      <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
+        <div className="flex flex-col gap-9">
+          {/* <!-- Input Fields --> */}
+          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+              <h3 className="font-medium text-black dark:text-white">
+                Input Fields
+              </h3>
+            </div>
+            <div className="flex flex-col gap-5.5 p-6.5">
+              <div>
+                <label className="mb-3 block text-black dark:text-white">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                />
+              </div>
+
+              <div>
+                <label className="mb-3 block text-black dark:text-white">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="w-full rounded-lg border-[1.5px] border-primary bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:bg-form-input dark:text-white"
+                />
+              </div>
+
+              <div>
+                <label className="mb-3 block font-medium text-black dark:text-white">
+                  Select Group
+                </label>
+                <input
+                  type="text"
+                  placeholder="Group"
+                  disabled
+                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary dark:disabled:bg-black"
+                />
+              </div>
+            </div>
+          </div>
+
+          
+          {/* <!-- File upload --> */}
+          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+              <h3 className="font-medium text-black dark:text-white">
+                File upload
+              </h3>
+            </div>
+            <div className="flex flex-col gap-5.5 p-6.5">
+              <div>
+                <label className="mb-3 block text-black dark:text-white">
+                  Attach file
+                </label>
+                <input
+                  type="file"
+                  className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+                />
+              </div>
+
+              <div>
+                <label className="mb-3 block text-black dark:text-white">
+                  Attach file
+                </label>
+                <input
+                  type="file"
+                  className="w-full rounded-md border border-stroke p-3 outline-none transition file:mr-4 file:rounded file:border-[0.5px] file:border-stroke file:bg-[#EEEEEE] file:py-1 file:px-2.5 file:text-sm focus:border-primary file:focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-strokedark dark:file:bg-white/30 dark:file:text-white"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-9">
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FormElements;
